cd3869c28d5f26f2f53205f34ca2a52287a495eb
internap627 <you@example.com>
2018-10-04 13:47:00 +0100

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,12 @@
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+  10.times do string = "s"+string
+  end
+  string
   else
   string
   end
 end
+
+
+# 10.times "s" + string
--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,9 +9,15 @@ def turtles
 end
 
 def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
-    end
+  arrNew = []
+  turtles.each do |turtle|
+    if turtle[:traits].class == String
+      arr = []
+      arr.push(turtle[:traits])
+      arrNew.push(arr)
+    else
+    arrNew.push(turtle[:traits])
   end
+  end
+  arrNew
 end
