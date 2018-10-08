39b55cf02e98a527edb39c7c972512d83715442b
deedeeh <dinahafez.8@gmail.com>
2018-10-08 09:37:39 +0100

    Done

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,9 +9,19 @@ def turtles
 end
 
 def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
+  hash={}
+  arr=[]
+  turtles.each do |turtle|
+    hash[turtle[:name]]=turtle[:traits]
+  end
+  hash.each do |turtle,traits|
+    if traits.is_a?(Array)
+      arr<<traits
+    else
+      brr=[]
+      brr<<traits
+      arr<<brr
     end
   end
+  arr
 end
